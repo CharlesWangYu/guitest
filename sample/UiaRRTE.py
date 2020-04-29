4006b218c508d5308ed6c63b4a08fa62ff13c32e
WANGYU <daminggong@aliyun.com>
2020-04-29 19:12:25 +0800

    Add searching operation of TextBlock control.

--- sample/UiaRRTE.py
+++ sample/UiaRRTE.py
@@ -1,10 +1,14 @@
 #from comtypes import GUID
 from comtypes.client import *
 from ctypes import *
+from comtypes.gen.UIAutomationClient import *
+from ctypes.wintypes import tagPOINT
+import comtypes
 
 # Start ui automation
 UIAutomationClient = GetModule("UIAutomationCore.dll")
 IUIAutomation = CreateObject("{ff48dba4-60ef-4201-aa87-54103eef594e}", interface=UIAutomationClient.IUIAutomation)
+walker = IUIAutomation.RawViewWalker
 
 # Get root node(desktop)
 root = IUIAutomation.GetRootElement()
@@ -26,4 +30,36 @@ btn = root.FindFirst(UIAutomationClient.TreeScope_Descendants, cnd)
 pattern = btn.GetCurrentPattern(UIAutomationClient.UIA_InvokePatternId)
 #print (pattern)
 ctrl = cast(pattern, POINTER(UIAutomationClient.IUIAutomationInvokePattern))
+#ctrl.Invoke()
+
+# Get parent & grand element of Import Package/DD button
+parent = walker.GetParentElement(btn)
+#grand = walker.GetParentElement(parent)
+#print(grand.CurrentProcessId)
+#print(grand.CurrentName)
+
+# Find "Online" TAB button and push it
+cnd = IUIAutomation.CreatePropertyConditionEx(UIAutomationClient.UIA_NamePropertyId,
+                                                'Online',
+												UIAutomationClient.PropertyConditionFlags_None)
+txt = root.FindFirst(UIAutomationClient.TreeScope_Descendants, cnd)
+btn_online = walker.GetParentElement(txt)
+pattern = btn_online.GetCurrentPattern(UIAutomationClient.UIA_InvokePatternId)
+ctrl = cast(pattern, POINTER(UIAutomationClient.IUIAutomationInvokePattern))
 ctrl.Invoke()
+#print(txt.CurrentName)
+#print(btn_online.CurrentName)
+
+# Find TreeView root node
+cnd = IUIAutomation.CreatePropertyConditionEx(UIAutomationClient.UIA_NamePropertyId,
+                                                'Fdi.Ui.ViewModel.Layout.TableViewModel',
+												UIAutomationClient.PropertyConditionFlags_None)
+online_root = root.FindFirst(UIAutomationClient.TreeScope_Descendants, cnd)
+
+# Find PV node in online TAB
+cnd = IUIAutomation.CreatePropertyConditionEx(UIAutomationClient.UIA_NamePropertyId,
+                                                'PV',
+												UIAutomationClient.PropertyConditionFlags_None)
+pv = online_root.FindFirst(UIAutomationClient.TreeScope_Descendants, cnd)
+print(pv.CurrentProcessId)
+print(pv.CurrentName)
