ab94d1b787e99a18d9524f34bee9ecfc5d93886f
Wang.Yu <ouu_cn@163.com>
2020-06-13 15:54:25 +0800

    add uia2.py

--- source/uia2.py
+++ source/uia2.py
@@ -1,26 +1,34 @@
-import pdb
-import logging
-import os
-import sys
-import time
-from configparser import ConfigParser
+#import pdb
+#import logging
+#import time
 from comtypes.client import *
 from ctypes import *
 
-UIAClient = GetModule('UIAutomationCore.dll')
-IUIA = CreateObject('{ff48dba4-60ef-4201-aa87-54103eef594e}', interface=UIAClient.IUIAutomation)
+# This is the operation of UIA initialization, they need to be executed 
+# immediately when starting this module.
+UIA_CORE_NAME			= 'UIAutomationCore.dll'
+UIA_OBJ_ID				= '{ff48dba4-60ef-4201-aa87-54103eef594e}'
+
+UIAClient = GetModule(UIA_CORE_NAME)
+IUIA = CreateObject(UIA_OBJ_ID, interface=UIAClient.IUIAutomation)
 DesktopRoot = IUIA.GetRootElement()
 
+# These are some commonly used UIA constant definitions.
 SCOPE_CHILDREN			= UIAClient.TreeScope_Children
 SCOPE_DESCENDANTS		= UIAClient.TreeScope_Descendants
 
+# This is a group of functions used to judge the attributes of UIA objects
 def isUIAElem(elem):
-	try:
-		temp = elem.CurrentName
-		return True
-	except Exception as e:
-		return False
+	NULL = cast(0, POINTER(UIAClient.IUIAutomationElement))
+	return not (elem == NULL)
 
+def isTreeLeaf(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_ExpandCollapsePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationExpandCollapsePattern))
+	return ctrl.value.CurrentExpandCollapseState == UIAClient.ExpandCollapseState_LeafNode
+
+# This is a group of functions for finding UIA objects.
 def findAllElem(root, key, type, scope=SCOPE_DESCENDANTS):
 	cnd = IUIA.CreatePropertyConditionEx(type, key, UIAClient.PropertyConditionFlags_None)
 	all = root.FindAll(scope, cnd)
@@ -79,29 +87,102 @@ def findFirstElemByControlType(root, type, scope=SCOPE_DESCENDANTS):
 def findFirstElemByAutomationId(root, Id, scope=SCOPE_DESCENDANTS):
 	return findFirstElem(root, Id, UIAClient.UIA_AutomationIdPropertyId, scope)
 
-def getParentElem(child):
+def findFirstElemBySubText(root, name, flag=UIAClient.PropertyConditionFlags_None, scope=UIAClient.TreeScope_Descendants):
+	child = findFirstElemByName(start, name, SCOPE_CHILDREN)
+	element = getParentElem(child)
+	return element
+
+def findParentElem(child):
 	walker = IUIA.ControlViewWalker
 	parent = walker.GetParentElement(child)
 	assert isUIAElem(parent)
 	return parent
 
-def getNextSiblingElem(elem):
+def findFirstChildElem(elem):
+	walker = IUIA.ControlViewWalker
+	first = walker.GetFirstChildElement(elem)
+	assert isUIAElem(first)
+	return first
+
+def findLastChildElem(elem):
+	walker = IUIA.ControlViewWalker
+	last = walker.GetLastChildElement(elem)
+	assert isUIAElem(last)
+	return last
+
+def findNextSiblingElem(elem):
 	walker = IUIA.ControlViewWalker
 	next = walker.GetNextSiblingElement(elem)
 	assert isUIAElem(next)
 	return next
 
-def getPreviousSiblingElem(elem):
+def findPreviousSiblingElem(elem):
 	walker = IUIA.ControlViewWalker
 	element = walker.GetPreviousSiblingElement(elem)
 	assert isUIAElem(next)
 	return element
 
+# This is a group of functions used to obtain UIA object attributes.
 def getElemSubName(elem):
-	text = findFirstElem(elem, Client.UIA_TextControlTypeId, Client.UIA_ControlTypePropertyId, scope=SCOPE_CHILDREN)
+	text = findFirstElem(elem, UIAClient.UIA_TextControlTypeId, UIAClient.UIA_ControlTypePropertyId, scope=SCOPE_CHILDREN)
 	return text.CurrentName
 
-def findFirstElemBySubText(root, name, flag=Client.PropertyConditionFlags_None, scope=Client.TreeScope_Descendants):
-	child = findFirstElem(start, name, Client.UIA_NamePropertyId)
-	element = getParentElem(child)
-	return element
+# This is a group of functions for manipulating UIA objects.
+def setEditbox(elem, text):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_ValuePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationValuePattern))
+	elem.SetFocus()
+	ctrl.SetValue(text)
+
+def expandTree(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_ExpandCollapsePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationExpandCollapsePattern))
+	if ctrl.value.CurrentExpandCollapseState == UIAClient.ExpandCollapseState_Collapsed:
+		ctrl.Select()
+
+def collapseTree(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_ExpandCollapsePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationExpandCollapsePattern))
+	if ctrl.value.CurrentExpandCollapseState == UIAClient.ExpandCollapseState_Expanded:
+		ctrl.Select()
+
+def pushLeaf(elem):
+	assert isTreeLeaf(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_SelectionItemPatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationSelectionItemPattern))
+	ctrl.Select()
+
+def pushButton(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_InvokePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationInvokePattern))
+	ctrl.Invoke()
+
+def selectTab(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_SelectionItemPatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationSelectionItemPattern))
+	if not ctrl.value.CurrentIsSelected:
+		ctrl.Select()
+
+def selectCheckbox(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_TogglePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationTogglePattern))
+	if not ctrl.value.CurrentToggleState:
+		ctrl.Toggle()
+
+def unselectCheckbox(elem):
+	assert isUIAElem(elem)
+	pattern = elem.GetCurrentPattern(UIAClient.UIA_TogglePatternId)
+	ctrl = cast(pattern, POINTER(UIAClient.IUIAutomationTogglePattern))
+	if ctrl.value.CurrentToggleState:
+		ctrl.Toggle()
+
+if __name__ == '__main__':
+	#test = findFirstElemByName(DesktopRoot, 'XXXXXXXXX')
+	test = findFirstElemByControlType(DesktopRoot, UIAClient.UIA_WindowControlTypeId)
+	assert isUIAElem(test)
